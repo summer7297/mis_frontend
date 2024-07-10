d1958850da15bee37a4be3c4c6528b999dee3bd1
yunuchoi <yoonwoochoi00@gmail.com>
2024-07-10 22:55:39 +1200

    Fix test issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { LoginForm } from "./components/LoginForm";
+import App from "./App";
 
 
 test('renders correctly', () => {
-const component = renderer.create(<LoginForm />);
+const component = renderer.create(<App />);
 const tree = component.toJSON();
 expect(tree).toMatchSnapshot();
 });
