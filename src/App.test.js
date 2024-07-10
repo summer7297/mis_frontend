588f4992f0c672a3f2b3c495725fba1c80cbb240
yunuchoi <yoonwoochoi00@gmail.com>
2024-07-10 22:54:09 +1200

    Fix test issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderer from 'react-test-renderer';
+import { LoginForm } from "./components/LoginForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('renders correctly', () => {
+const component = renderer.create(<LoginForm />);
+const tree = component.toJSON();
+expect(tree).toMatchSnapshot();
 });
