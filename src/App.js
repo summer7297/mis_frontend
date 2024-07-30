99050507e83b1f74e5f59b111220248f3dd5e02c
yunuchoi <yoonwoochoi00@gmail.com>
2024-07-30 20:24:56 +1200

    prepare for Assignment 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
 
-import { Login } from "./pages/Login"
 import { Register } from "./pages/Register";
 import { Dashboard } from "./pages/Dashboard";
 
@@ -25,9 +24,8 @@ function App() {
       <ThemeProvider theme={theme}>
           <Router>
               <Routes>
-                  <Route path="/" element={<Login />} />
+                  <Route path="/" element={<Dashboard />} />
                   <Route path="/register" element={<Register />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
               </Routes>
           </Router>
       </ThemeProvider>
--- src/pages/Login.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { LoginForm } from "../components/LoginForm"
-
-export const Login = () => (<LoginForm />)
